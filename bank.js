c0105a21af1d5e3f6e7bb11c86528c721248d78a
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-02-13 23:04:14 +0600

    update panda commerce

--- bank.js
+++ bank.js
@@ -1,70 +1,85 @@
 
-//login button event handelar
-const loginArea = document.getElementById('login');
+  //login button event handelar
+  const loginArea = document.getElementById('login');
 
-loginArea.addEventListener('click',function(){
+  loginArea.addEventListener('click',function(){
 
-const submiBtn = document.getElementById('login-area');
+    const emailInput = document.getElementById('mail-btn');
+    const email = emailInput.value;
+    const passewordInput = document.getElementById('pass-btn');
+    const password = passewordInput.value;
+    if(email == 'sadiq@gmail.com' && password == 'sadiq')
+    {
+        const transaction = document.getElementById('transaction-area');
 
-submiBtn.style.display = 'none';
+          transaction.style.display = 'block';
 
-const transaction = document.getElementById('transaction-area');
+            const submiBtn = document.getElementById('login-area');
+            submiBtn.style.display = 'none';
+    }
+    else{
+      
+      alert ('Pless Enter your rigth email and pass')
 
-transaction.style.display = 'block';
-
-})
-
-
-//deposite button event handelar
-const deposite = document.getElementById('addDeposit');
-//console.log(deposite)
-
-deposite.addEventListener('click',function(){
-const depositeNumber = getInputNumber('depositAmount')
-
-  // const depositeAmount = document.getElementById('depositAmount').value;
-  // const depositeNumber = parseFloat(depositeAmount);
-  //console.log(depositeNumber)
+        }
 
 
-//   const currentDeposit = document.getElementById('currentDposit').innerText;
-//   const currentDepositNumber = parseFloat(currentDeposit);
-//  const totalDeposit = depositeNumber + currentDepositNumber;
-//  document.getElementById('currentDposit').innerText = totalDeposit;
 
-//  const currentBalance =  document.getElementById('currentBalance').innerText;
-//  const currentBalanceNumber = parseFloat(currentBalance);
-//  const totalBalance = currentBalanceNumber +  depositeNumber ;
-//  document.getElementById('currentBalance').innerText = totalBalance;
+  
 
-updateSpanText('currentDeposit',depositeNumber);
-updateSpanText('currentBalance',depositeNumber);
-document.getElementById('depositAmount').value = '';
-})
+  })
 
-//withDraw event Handelar
 
-const withdrawBtn = document.getElementById('addWithdraw');
-withdrawBtn.addEventListener('click',function(){
-const withdrawNumber = getInputNumber('withdrawAmount');
+  //deposite button event handelar
+  const deposite = document.getElementById('addDeposit');
+  //console.log(deposite)
 
-updateSpanText('currentWithdraw',withdrawNumber);
-updateSpanText('currentBalance', -1*withdrawNumber);
+  deposite.addEventListener('click',function(){
+  const depositeNumber = getInputNumber('depositAmount')
 
-document.getElementById('withdrawAmount').value = '';
-})
+    // const depositeAmount = document.getElementById('depositAmount').value;
+    // const depositeNumber = parseFloat(depositeAmount);
+    //console.log(depositeNumber)
 
-// withdraw event function
-function getInputNumber(id){
-  const amount = document.getElementById(id).value;
-  const amountNumber = parseFloat(amount);
-  return amountNumber;
-}
 
-//add deposit and balance function
-function updateSpanText(id,addedNumber){
-  const currentBalance =  document.getElementById(id).innerText;
- const currentBalanceNumber = parseFloat(currentBalance);
- const totalBalance = currentBalanceNumber + addedNumber ;
- document.getElementById(id).innerText = totalBalance;
-}
+  //   const currentDeposit = document.getElementById('currentDposit').innerText;
+  //   const currentDepositNumber = parseFloat(currentDeposit);
+  //  const totalDeposit = depositeNumber + currentDepositNumber;
+  //  document.getElementById('currentDposit').innerText = totalDeposit;
+
+  //  const currentBalance =  document.getElementById('currentBalance').innerText;
+  //  const currentBalanceNumber = parseFloat(currentBalance);
+  //  const totalBalance = currentBalanceNumber +  depositeNumber ;
+  //  document.getElementById('currentBalance').innerText = totalBalance;
+
+  updateSpanText('currentDeposit',depositeNumber);
+  updateSpanText('currentBalance',depositeNumber);
+  document.getElementById('depositAmount').value = '';
+  })
+
+  //withDraw event Handelar
+
+  const withdrawBtn = document.getElementById('addWithdraw');
+  withdrawBtn.addEventListener('click',function(){
+  const withdrawNumber = getInputNumber('withdrawAmount');
+
+  updateSpanText('currentWithdraw',withdrawNumber);
+  updateSpanText('currentBalance', -1*withdrawNumber);
+
+  document.getElementById('withdrawAmount').value = '';
+  })
+
+  // withdraw event function
+  function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+  }
+
+  //add deposit and balance function
+  function updateSpanText(id,addedNumber){
+    const currentBalance =  document.getElementById(id).innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+  const totalBalance = currentBalanceNumber + addedNumber ;
+  document.getElementById(id).innerText = totalBalance;
+  }
